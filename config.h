358d435a8db868d5d373035f6fc81654f7135359
Z Linux <zlinux20@gmail.com>
2016-01-17 01:52:33 +0530

    Added the shortcut for letter "i" to toggle emacs tag no. 3

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 
 /* appearance */
 /* static const char font[]            = "Sans 8";*/
-/* static const char font[]            = "century gothic 11";*/
-static const char font[]            = "M+ 1c light 11";
+static const char font[]            = "century gothic 11";
+/*static const char font[]            = "M+ 1c light 11";*/
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
@@ -17,19 +17,22 @@ static const Bool topbar            = True;     /* False means bottom bar */
 static const Bool statusmarkup      = True;     /* True means use pango markup in status message */
 
 /* tagging */
-static const char *tags[] = { "1:web", "2:Term", "3:files", "4:Gvim", "5:Emacs", "6", "7:Gimp", "8:Inkscape", "9:Office" };
+static const char *tags[] = { "1:web", "2:Term", "3:Emacs", "4:Gvim", "5:Files", "6:Zathura", "7:Gimp", "8:Inkscape", "9:Office" };
 
 static const Rule rules[] = {
 	/* class        instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",              NULL,       NULL,       1 << 6,       False,       -1 },
-	{ "Inkscape",          NULL,       NULL,       1 << 7,       False,       -1 },
 	{ "Terminator",        NULL,       NULL,       1 << 1,       False,       -1 },
+	{ "Emacs",             NULL,       NULL,       1 << 2,       False,       -1 },
+	{ "chromium-browser",  NULL,       NULL,       1 << 3,       False,       -1 },
+	{ "Gvim",              NULL,       NULL,       1 << 3,       False,       -1 },
+	{ "Thunar",            NULL,       NULL,       1 << 4,       False,       -1 },
+	{ "Zathura",           NULL,       NULL,       1 << 5,       False,       -1 },
 	{ "Transmission-gtk",  NULL,       NULL,       1 << 5,       False,       -1 },
 	{ "Uget-gtk",          NULL,       NULL,       1 << 5,       False,       -1 },
-	{ "chromium-browser",  NULL,       NULL,       1 << 3,       False,       -1 },
-	{ "Emacs",             NULL,       NULL,       1 << 4,       False,       -1 },
-	{ "Gvim",              NULL,       NULL,       1 << 3,       False,       -1 },
+	{ "Gimp",              NULL,       NULL,       1 << 6,       False,       -1 },
+	{ "Inkscape",          NULL,       NULL,       1 << 7,       False,       -1 },
 	{ "mplayer2",          NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Vlc",               NULL,       NULL,       1 << 8,       False,       -1 },
 };
 
 /* layout(s) */
@@ -68,17 +71,17 @@ static const char *terminator[] = { "terminator", NULL, NULL, NULL,  "Terminator
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	/*{ ALTMODKEY,                    XK_p,      spawn,          {.v = dmenucmd } },*/
-	{ 0,                            XK_Print,  spawn,    SHCMD("exec scrot -q 90 -t 25 '%Y-%m-%d-%H-%M-%S.png' -e 'mv $f ~/.scrots'") }, 
+	{ 0,                            XK_Print,  spawn,          SHCMD("exec scrot -q 90 '%Y-%m-%d-%H-%M-%S.png' -e 'mv $f ~/.scrots'") }, 
 	{ ALTMODKEY,                    XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ ALTMODKEY,                    XK_p,      spawn,          {.v = rofiruncmd } },
+	{ ALTMODKEY|ShiftMask,          XK_d,      incnmaster,     {.i = -1 } },
+	{ ALTMODKEY|ShiftMask,          XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
@@ -104,8 +107,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	/* mapping the letter key 'o' to easily toggle the terminator tag */
+	/* mapping the letter key 'o' to easily toggle the terminator tag no. 2 */
 	TAGKEYS(                        XK_o,                      1)
+	/* mapping the letter key 'i' to easily toggle the emacs tag no. 3 */
+	TAGKEYS(                        XK_i,                      2)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
